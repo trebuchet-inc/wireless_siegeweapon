a755b918c50faca3f109cc009d6ed4fa2220f233
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-10-26 15:55:42 -0400

    maj

---
+++ Assets/_Script/Networking/Editor/NetworkTrackerComponentEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(NetworkTrackerComponent))]
+ public class MyScriptEditor : Editor
+ {
+   	void OnInspectorGUI()
+   	{
+     	NetworkTrackerComponent myScript = target as NetworkTrackerComponent;
+ 
+     	myScript.objectToTrack = EditorGUILayout.EnumPopup("Object To Track", ObjectToTrack, null);
+     
+    	if(myScript.objectToTrack == ObjectToTrack.Custom){}
+ 
+   	}
+}
---
+++ Assets/_Script/Networking/NetworkTrackerComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectToTrack 
+{
+	NVR_Head,
+	NVR_Hand,
+	Custom
+}
+
+public class NetworkTrackerComponent : MonoBehaviour 
+{
+	public ObjectToTrack objectToTrack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
