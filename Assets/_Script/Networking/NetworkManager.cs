f7c472cd355d32d5449b14fe2aae0591ce6b5d76
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-10-27 15:15:50 -0400

    maj

--- Assets/_Script/Networking/NetworkManager.cs
+++ Assets/_Script/Networking/NetworkManager.cs
@@ -36,8 +36,12 @@ public class NetworkManager : Photon.PunBehaviour
 
     public override void OnJoinedRoom()
     {
-        GameObject o = PhotonNetwork.Instantiate("RemotePlayer", Vector3.zero, Quaternion.identity, 0);
+        GameObject o = PhotonNetwork.Instantiate("Head", Vector3.zero, Quaternion.identity, 0);
+        GameObject o1 = PhotonNetwork.Instantiate("leftHand", Vector3.zero, Quaternion.identity, 0);
+        GameObject o2 = PhotonNetwork.Instantiate("rightHand", Vector3.zero, Quaternion.identity, 0);
         Destroy(o);
+        Destroy(o1);
+        Destroy(o2);
     }
 
     public void OnGUI()
--- Assets/_Script/Networking/NetworkTrackerComponent.cs
+++ Assets/_Script/Networking/NetworkTrackerComponent.cs
@@ -44,7 +44,7 @@ public class NetworkTrackerComponent : Photon.MonoBehaviour
 	}
 	
 	void Update () {
-		if (!_masterPhotonView.isMine)
+		if (!photonView.isMine)
 		{
 			transform.position = Vector3.Lerp(transform.position, this._correctPlayerPos, Time.deltaTime * 10);
 			transform.rotation = Quaternion.Lerp(transform.rotation, this._correctPlayerRot, Time.deltaTime * 10);
