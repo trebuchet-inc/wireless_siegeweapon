2ddf6d737c32012d58bf2d1a856162dc2498c6ff
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-10-30 12:43:22 -0400

    maj

--- Assets/_Script/Networking/NetworkPlayerComponent.cs
+++ Assets/_Script/Networking/NetworkPlayerComponent.cs
@@ -13,7 +13,27 @@ public class NetworkPlayerComponent : MonoBehaviour
 	void Start()
 	{
 		_playerpart = new GameObject[3];
+		setPlayerPart();
+	}
 
+	void FixedUpdate()
+	{
+		for(int i = 0; i < _playerpart.Length; i++)
+		{
+			if(_playerpart != null)
+			{
+				_playerpart[i].transform.position = Vector3.Lerp(_playerpart[i].transform.position, targetValues.positions[i].Deserialize(), Time.deltaTime * 10);
+				_playerpart[i].transform.rotation = Quaternion.Lerp(_playerpart[i].transform.rotation, targetValues.rotations[i].Deserialize(), Time.deltaTime * 10);
+			}
+			else
+			{
+				setPlayerPart();
+			} 
+		}
+	}
+
+	void setPlayerPart()
+	{
 		for(int i = 0; i < transform.childCount; i++)
 		{
 			NVRHand hand = transform.GetChild(i).GetComponent<NVRHand>();
@@ -22,14 +42,6 @@ public class NetworkPlayerComponent : MonoBehaviour
 			if(hand.IsRight) _playerpart[1] = transform.GetChild(i).gameObject;
 			else _playerpart[2] = transform.GetChild(i).gameObject;
 		}
-	}
-
-	void FixedUpdate()
-	{
-		for(int i = 0; i < _playerpart.Length; i++)
-		{
-			_playerpart[i].transform.position = Vector3.Lerp(_playerpart[i].transform.position, targetValues.positions[i].Deserialize(), Time.deltaTime * 10);
-			_playerpart[i].transform.rotation = Quaternion.Lerp(_playerpart[i].transform.rotation, targetValues.rotations[i].Deserialize(), Time.deltaTime * 10);
-		}
+		print("setPlayerPart");
 	}
 }
