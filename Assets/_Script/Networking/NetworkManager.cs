7a2e8ed1d90a4081d29967923088bf1db7849236
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-11-01 13:32:35 -0400

    squirel

--- Assets/_Script/Networking/NetworkManager.cs
+++ Assets/_Script/Networking/NetworkManager.cs
@@ -14,7 +14,9 @@ public class NetworkManager : Photon.PunBehaviour
 
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings("0.1");
+        PhotonNetwork.ConnectToMaster("74.58.238.88", 5056, "64d0546d-f744-41eb-8817-1db17103b312", "0.1");
+        //PhotonNetwork.ConnectToMaster("192.168.0.127", 5056, "64d0546d-f744-41eb-8817-1db17103b312", "0.1");
+        //PhotonNetwork.ConnectToMaster("127.0.0.1", 5055, "64d0546d-f744-41eb-8817-1db17103b312", "0.1");
     }
 
     public override void OnJoinedLobby()
@@ -25,6 +27,7 @@ public class NetworkManager : Photon.PunBehaviour
 
     public override void OnConnectedToMaster()
     {
+        Debug.Log("Connected");
         PhotonNetwork.JoinRandomRoom();
     }
 
